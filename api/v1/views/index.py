a7168086e97e9b3ea9d0dfe6c7c4eed7c4e6bd16
vergilx6x <med.work96@hotmail.com>
2024-08-03 18:26:00 +0100

    Adding stats api feature

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,15 @@ Index page
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Returns status code in json"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def stats():
+    """Return the number of each object by type."""
+    return jsonify(storage.count)
