7a6742c0bef13fa0aef00694f4c59b3219d529e1
vergilx6x <med.work96@hotmail.com>
2024-08-04 18:50:16 +0100

    task 8

---
+++ dump.sh
@@ -0,0 +1,3 @@
+INSERT INTO states (name, id, created_at, updated_at) VALUES ('California', 'CA123', NOW(), NOW());
+INSERT INTO states (name, id, created_at, updated_at) VALUES ('Texas', 'TX456', NOW(), NOW());
+INSERT INTO states (name, id, created_at, updated_at) VALUES ('New York', 'NY789', NOW(), NOW());
