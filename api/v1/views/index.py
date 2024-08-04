be26ad3c1ac858707d07ad32f2890ab4dd10f0b8
Egba Frank <Egbafrank72@gmail.com>
2024-08-04 06:56:03 +0000

    Updated state api and count logic

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,14 @@
 Index page
 """
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for States """
+""" methods to handle all default RESTFul API actions for States """
 from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
+from flask import abort, jsonify, request
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', strict_slashes=False)
 def get_states():
     """
     Retrieves the list of all State objects
     """
-    all_states = storage.all(State).values()
-    list_states = []
-    for state in all_states:
-        list_states.append(state.to_dict())
+    states = storage.all(State).values()
+    list_states = [state.to_dict() for state in states]
+    
     return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', strict_slashes=False)
 def get_state(state_id):
     """ Retrieves a specific State """
     state = storage.get(State, state_id)
@@ -43,28 +42,28 @@ def delete_state(state_id):
     storage.delete(state)
     storage.save()
 
-    return make_response(jsonify({}), 200)
+    return jsonify({})
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_state():
+def create_state():
     """
     Creates a State
     """
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    data = request.get_json(silent=True)
+    if not data:
+        return jsonify({"error": "Not a JSON"}), 400
 
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
+    if 'name' not in data:
+        return jsonify({"error": "Missing name"}), 400
 
-    data = request.get_json()
-    instance = State(**data)
-    instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def put_state(state_id):
+def update_state(state_id):
     """
     Updates a State
     """
@@ -73,14 +72,17 @@ def put_state(state_id):
     if not state:
         abort(404)
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    data = request.get_json(silent=True)
+
+    if not data:
+        return jsonify({"error": "Not a JSON"}), 400
 
     ignore = ['id', 'created_at', 'updated_at']
 
-    data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
             setattr(state, key, value)
+
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+
+    return jsonify(state.to_dict())
