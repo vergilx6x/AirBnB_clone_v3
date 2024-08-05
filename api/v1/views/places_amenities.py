0994c5fb55768dca47c8ef1cde8dfd8d00564982
vergilx6x <med.work96@hotmail.com>
2024-08-05 19:49:01 +0000

    Fix

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -6,12 +6,10 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/get_places_amenities.yml',
            methods=['GET'])
 def get_place_amenities(place_id):
     """
@@ -33,7 +31,6 @@ def get_place_amenities(place_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/place_amenity/delete_place_amenities.yml',
            methods=['DELETE'])
 def delete_place_amenity(place_id, amenity_id):
     """
@@ -64,7 +61,6 @@ def delete_place_amenity(place_id, amenity_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/post_place_amenities.yml',
            methods=['POST'])
 def post_place_amenity(place_id, amenity_id):
     """
